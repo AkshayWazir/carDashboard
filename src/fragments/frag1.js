262270ba578cf9a9e1819ad5b3b4dd78f4a7cdeb
akshay.rein-at-908805445373 <waizaeer4@gmail.com>
2022-06-21 15:03:28 +0530

    ttt

--- src/fragments/frag1.js
+++ src/fragments/frag1.js
@@ -1,7 +1,20 @@
 import React from 'react';
-import {View, StyleSheet} from 'react-native';
+import {View, StyleSheet, Button} from 'react-native';
+import Animated, {
+  useSharedValue,
+  useAnimatedStyle,
+  withTiming,
+} from 'react-native-reanimated';
+
+function animateTO(property, targetValue) {
+  property.value = withTiming(targetValue, {duration: 500});
+}
 
 export default function Frag1(props) {
+  const leftMargin = useSharedValue(0);
+  const animatedSty = useAnimatedStyle(() => {
+    return {marginLeft: leftMargin.value};
+  });
   const styles = StyleSheet.create({
     mainCont: {
       height: '100%',
@@ -9,5 +22,18 @@ export default function Frag1(props) {
       backgroundColor: '#00ff00',
     },
   });
-  return <View style={styles.mainCont}></View>;
+
+  const initStyle = StyleSheet.create({
+    initial: {
+      height: 500,
+      width: 500,
+      backgroundColor: '#ff00ff',
+    },
+  });
+  return (
+    <View style={styles.mainCont}>
+      <Animated.View style={[initStyle.initial, animatedSty]} />
+      <Button title="Animate Now" onPress={() => animateTO(leftMargin, 200)} />
+    </View>
+  );
 }
