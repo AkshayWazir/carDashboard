a7e00fec96137f530eef70ec6d745b91477b6751
akshay.rein-at-908805445373 <waizaeer4@gmail.com>
2022-06-22 09:35:39 +0530

    test

--- src/fragments/frag1.js
+++ src/fragments/frag1.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useState} from 'react';
 import {View, StyleSheet, Button} from 'react-native';
 import Animated, {
   useSharedValue,
@@ -11,29 +11,47 @@ function animateTO(property, targetValue) {
 }
 
 export default function Frag1(props) {
-  const leftMargin = useSharedValue(0);
+  const [value, setValue] = useState(200);
+
+  // Animated Value
+  const leftMargin = useSharedValue(0),
+    height = useSharedValue(500),
+    width = useSharedValue(500);
+
+  // Animated Styles result
   const animatedSty = useAnimatedStyle(() => {
-    return {marginLeft: leftMargin.value};
+    return {
+      marginLeft: leftMargin.value,
+      height: height.value,
+      width: width.value,
+    };
   });
+
   const styles = StyleSheet.create({
     mainCont: {
       height: '100%',
       width: '100%',
       backgroundColor: '#00ff00',
     },
-  });
-
-  const initStyle = StyleSheet.create({
     initial: {
       height: 500,
       width: 500,
       backgroundColor: '#ff00ff',
     },
   });
+
   return (
     <View style={styles.mainCont}>
-      <Animated.View style={[initStyle.initial, animatedSty]} />
-      <Button title="Animate Now" onPress={() => animateTO(leftMargin, 200)} />
+      <Animated.View style={[styles.initial, animatedSty]} />
+      <Button
+        title="Animate Now"
+        onPress={() => {
+          animateTO(leftMargin, value);
+          animateTO(height, value);
+          animateTO(width, value);
+          setValue(value * -1);
+        }}
+      />
     </View>
   );
 }
--- src/fragments/frag2.js
+++ src/fragments/frag2.js
@@ -1,13 +1,43 @@
 import React from 'react';
-import {View, StyleSheet} from 'react-native';
+import {View, StyleSheet, Button} from 'react-native';
+import Animated, {
+  useSharedValue,
+  useAnimatedStyle,
+  withTiming,
+} from 'react-native-reanimated';
 
 export default function Frag2(props) {
+  const animLeftMargin = useSharedValue(10);
+  const animTopMargin = useSharedValue(10);
+
+  const animatedStyles = useAnimatedStyle(() => {
+    return {marginLeft: animLeftMargin.value, marginTop: animTopMargin.value};
+  });
+
   const styles = StyleSheet.create({
     mainCont: {
       height: '100%',
       width: '100%',
-      backgroundColor: '#ff0000',
+      backgroundColor: '#fff',
+    },
+    circle: {
+      height: 400,
+      width: 400,
+      borderRadius: 400,
+      backgroundColor: '#ff00ff',
+      margin: 10,
     },
   });
-  return <View style={styles.mainCont}></View>;
+  return (
+    <View style={styles.mainCont}>
+      <Button
+        title="animate"
+        onPress={() => {
+          animLeftMargin.value = withTiming(200, {duration: 1000});
+          animTopMargin.value = withTiming(200, {duration: 1000});
+        }}
+      />
+      <Animated.View style={[styles.circle, animatedStyles]} />
+    </View>
+  );
 }
